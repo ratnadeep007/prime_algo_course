b7eeda494496058adf46d9829e91f7ad90988340
Ratnadeep Bhattacharyya <ratnadeep.bhattacharyya@dentsu.com>
2023-05-27 18:26:58 +0530

    data structures

---
+++ src/day1/Stack.ts
@@ -0,0 +1,44 @@
+class Node<T> {
+  value: T;
+  prev?: Node<T>;
+}
+
+export default class Stack<T> {
+  public length: number;
+  private head?: Node<T>;
+
+
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
+
+  push(item: T): void {
+    const node = { value: item } as Node<T>;
+    this.length++;
+    if (!this.head) {
+      this.head = node;
+      return;
+    } 
+
+    node.prev = this.head;
+    this.head = node;
+  }
+  pop(): T | undefined {
+    this.length = Math.max(0, this.length--);
+    if (!this.head) {
+      this.length = 0;
+      return undefined;
+    }
+    let node = this.head;
+    this.head = node.prev;
+    this.length--;
+
+    node.prev = undefined;
+
+    return node.value;
+  }
+  peek(): T | undefined {
+    return this.head?.value;
+  }
+}
